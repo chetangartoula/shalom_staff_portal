f219ff4a8272d4e808e8eedc3325eba57f924531
simple kto <simplekto@gmail.com>
2025-11-18 06:49:47 +0000

    logo is not loaded use this link for geting the logo image https://www.s

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.shalomtreks.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   env: {
