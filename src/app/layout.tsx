9696436bfaa3cfd215040f474fc000754fb8b34a
Chetan Gartoula <gartoularachit@gmail.com>
2025-12-24 18:53:12 +0545

    Comment out PWAInstallPrompt in layout as required

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           {children}
         </ReactQueryProvider>
         <PWAManager />
-        <PWAInstallPrompt />
+        {/* <PWAInstallPrompt /> */}
         <Toaster />
       </body>
     </html>
